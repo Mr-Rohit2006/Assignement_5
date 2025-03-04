6788ae7c69dbd30e26b7c5555039db30635e651a
Rohit Goyal <163022120+Mr-Rohit2006@users.noreply.github.com>
2025-03-04 10:18:14 +0530

    Add files via upload

---
+++ Assignment 5/Task_2.py
@@ -0,0 +1,9 @@
+
+numbers = list(range(1, 11))
+
+extracted_list = numbers[:5]
+
+reversed_list = extracted_list[::-1]
+print("Original list:", numbers)
+print("Extracted first five elements:", extracted_list)
+print("Reversed extracted elements:", reversed_list)
